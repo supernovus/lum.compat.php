49ab3b5fa9aa1ae61966f7fa23c21117d82ad44a
Timothy Totten <2010@huri.net>
2021-12-31 12:32:46 -0800

    Enhanced the Util library a bit.
    - Added Util::flagErr() method for flexible error handling.
    - Added Util::load_data_from() method which loads JSON/YAML files.
    - Reworked Util::load_opts_from() to use load_data_from().
    - Added return types to a few places.

--- lib/Util.php
+++ lib/Util.php
@@ -7,6 +7,28 @@ namespace Lum;
  */
 class Util
 {
+  const LOAD_MISSING_FATAL = 1;
+  const LOAD_MISSING_LOG   = 2;
+  const LOAD_INVALID_FATAL = 4;
+  const LOAD_INVALID_LOG   = 8;
+  const LOAD_EMPTY_FATAL   = 16;
+  const LOAD_EMPTY_LOG     = 32;
+  const LOAD_NOT_IT_FATAL  = 64;
+  const LOAD_NOT_IT_LOG    = 128;
+
+  const LOAD_OPTS_DEFAULTS
+    = self::LOAD_INVALID_FATAL
+    | self::LOAD_EMPTY_FATAL
+    | self::LOAD_NOT_IT_FATAL;
+
+  const LOAD_ALL_FATAL = self::LOAD_OPTS_DEFAULTS | self::LOAD_MISSING_FATAL;
+
+  const LOAD_ALL_LOG
+    = self::LOAD_MISSING_LOG
+    | self::LOAD_INVALID_LOG
+    | self::LOAD_EMPTY_LOG
+    | self::LOAD_NOT_IT_LOG;
+
   /**
    * Manipulate a set of binary flags.
    *
@@ -25,26 +47,69 @@ class Util
   }
 
   /**
-   * Populate some options in an array or array-like object based
-   * on the contents of a JSON or YAML configuration file.
+   * Handle errors differently depending on flags.
    *
-   * @param string $file       The filename we want to load.
-   * @param mixed  &$opts      The array we are reading the options into.
-   *   May be a PHP `array`, or an object implementing `ArrayAccess`.
-   * @param bool   $overwrite  Should we overwrite existing options?
+   * @param string $message  The error message.
+   * @param int $flags  Currently set flags.
+   * @param int $fflag  The flag indicating errors should be fatal.
+   * @param int $lflag  The flag indicating errors should be logged.
    *
-   * @return void
+   * @return null  This always returns null if fatal wasn't true.
+   *
+   * @throws Exception  This always throws if fatal was true.
    */
-  static function load_opts_from (string $file, &$opts, $overwrite=false)
+  static function flagErr (string $message, int $flags, 
+    int $fflag, int $lflag)
   {
+    $fatal = boolval($flags & $fflag);
+    $log   = boolval($flags & $lflag);
+
+    if ($fatal)
+    {
+      throw new Exception($message);
+    }
+
+    if ($log)
+    {
+      error_log($message);
+    }
+
+    return null;
+  }
+
+  /**
+   * Load a JSON or YAML file and return the contents.
+   * Uses a few forms of format auto-detection.
+   *
+   * @param string $file  The filename we want to load.
+   * @param int $flags    (Optional) Flags controlling how errors as handled.
+   *                      Default: `self::LOAD_ALL_FATAL`
+   *
+   *   I need to document this properly.
+   *
+   * @return ?iterable  Either a valid array/object or null.
+   */
+  static function load_data_from (string $file, 
+    int $flags=self::LOAD_ALL_FATAL): ?iterable
+  {
+    $err = function(string $m, int $f, int$l) use ($flags)
+    {
+      return static::flagErr($m, $flags, $f, $l);
+    };
+
     if (file_exists($file) && is_readable($file))
     {
       $text = trim(file_get_contents($file));
 
       if (empty($text))
-      { // Why would you have an 
-        throw new Exception("Cannot load empty file '$file'");
+      { // Why would you have an empty file? Who knows.
+        $fatal = self::LOAD_EMPTY_FATAL;
+        $log = self::LOAD_EMPTY_LOG; 
+        return $err("Cannot load empty file '$file'", $fatal, $log);
       }
+
+      $fatal = self::LOAD_INVALID_FATAL;
+      $log = self::LOAD_INVALID_LOG;
 
       $pi = pathinfo($file);
       if (isset($pi['extension']))
@@ -77,7 +142,7 @@ class Util
         }
         else
         { // If we've made it this far and haven't found anything...
-          throw new Exception("Unknown file format for '$file', cannot parse");
+          return $err("Unknown file format for '$file'", $fatal, $log);
         }
       }
 
@@ -86,7 +151,7 @@ class Util
         $conf = json_decode($text, true);
         if (!isset($conf))
         {
-          throw new Exception("Invalid JSON in '$file', cannot continue.");
+          return $err("Invalid JSON in '$file'", $fatal, $log);
         }
       }
       elseif ($F == 'y')
@@ -95,14 +160,56 @@ class Util
         $conf = $yaml->parse($text);
         if (!isset($conf))
         {
-          throw new Exception("The YAML parser returned null, cannot continue.");
+          return $err("The YAML parser returned null", $fatal, $log);
         }
       }
       else
-      {
+      { // This one will always throw, because WTF?!
         throw new Exception("How did you get here?");
       }
 
+      if (is_iterable($conf))
+      { // We got what we need.
+        return $conf;
+      }
+      else
+      {
+        $fatal = self::LOAD_NOT_IT_FATAL;
+        $log = self::LOAD_NOT_IT_LOG;
+        return $err("Data from '$file' was not iterable", $fatal, $log);
+      }
+    }
+    else
+    { // No file, no content.
+      $fatal = self::LOAD_MISSING_FATAL;
+      $log = self::LOAD_MISSING_LOG;
+      return $err("File '$file' not found", $fatal, $log);
+    }
+  }
+
+  /**
+   * Populate some options in an array or array-like object based
+   * on the contents of a JSON or YAML configuration file.
+   *
+   * @param string $file       The filename we want to load.
+   * @param mixed  &$opts      The array we are reading the options into.
+   *   May be a PHP `array`, or an object implementing `ArrayAccess`.
+   * @param bool   $overwrite  (Optional) Should we overwrite existing options?
+   *                           Default: `false`
+   * @param int    $flags      (Optional) Flags to send to `load_data_from`
+   *                           Default: `self::LOAD_OPTS_DEFAULTS`
+   *
+   * @return void
+   */
+  static function load_opts_from (
+    string $file, 
+    mixed &$opts, 
+    bool $overwrite=false,
+    int $flags=self::LOAD_OPTS_DEFAULTS): void
+  {
+    $conf = static::load_data_from($file, $flags);
+    if (isset($conf))
+    {
       foreach ($conf as $ckey => $cval)
       {
         if ($overwrite || !isset($opts[$ckey]))
@@ -122,7 +229,7 @@ class Util
    *
    * @return string  The output from the PHP file.
    */
-  static function get_php_content ($__view_file, $__view_data=NULL)
+  static function get_php_content ($__view_file, $__view_data=NULL): string
   { 
     // First, start saving the buffer.
     ob_start();
