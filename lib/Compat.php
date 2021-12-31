b7efdb47ff11e1522e25263de0c0ffe87e870b14
Timothy Totten <2010@huri.net>
2021-12-30 18:47:35 -0800

    Added Compat::uopz() method

--- lib/Compat.php
+++ lib/Compat.php
@@ -24,4 +24,27 @@ final class Compat
     return trigger_error($msg, E_USER_DEPRECATED);
   }
 
+  /**
+   * Check if 'uopz' is loaded.
+   *
+   * @param ?bool $allow_exit  (Optional) If set, call `uopz_allow_exit()`
+   *
+   * @return bool  Was uopz loaded?
+   */
+  static function uopz(?bool $allow_exit=null): bool
+  {
+    if (function_exists('uopz_allow_exit'))
+    { // If the function is found, assume the extension is loaded.
+      if (isset($allow_exit))
+      {
+        uopz_allow_exit($allow_exit);
+      }
+      return true;
+    }
+    else
+    { // No function, no extension.
+      return false;
+    }
+  }
+
 } // Compat class
